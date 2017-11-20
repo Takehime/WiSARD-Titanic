c976de2650ef08bde3250e5d660a21dd3c11cb05
Vinícius Garcia <vingarciasc@gmail.com>
2017-11-20 13:22:58 -0200

    v0.4.1
    cross-validation

--- Main.py
+++ Main.py
@@ -3,10 +3,7 @@ import numpy as np
 import Reader as r
 import sys
 
-def train_and_generate_test_prediction(num_bits_addr):
-    num_bits_addr = num_bits_addr
-    successes = 0
-    tries = 0
+def generate_test_prediction(num_bits_addr):
     X = []
     y = []
     binary, result = r.get_binary_passengers('Resources/train.csv')
@@ -15,7 +12,7 @@ def train_and_generate_test_prediction(num_bits_addr):
         X.append(binary[i])
         y.append(str(result[i]))
 
-    w = wi.WiSARD(num_bits_addr, False)
+    w = wi.WiSARD(num_bits_addr)
     w.fit(X, y)
 
     binary, result = r.get_binary_passengers('Resources/test.csv')
@@ -37,7 +34,7 @@ def train_and_generate_test_prediction(num_bits_addr):
     print("Done.")
     print("====================")        
 
-def train_and_local_test(num_bits, thorough_test):
+def local_test(num_bits, thorough_test):
     num_bits_addr = num_bits
     successes = 0
     tries = 0
@@ -45,19 +42,13 @@ def train_and_local_test(num_bits, thorough_test):
     y = []
     binary, result = r.get_binary_passengers('Resources/train.csv')
 
-    order = []
-    for i in range(0, len(binary) - 1):
-        order.append(i)
-
-    np.random.shuffle(order)
-
-    for i in range(0, int(0.9*len(binary))):
-        X.append(binary[order[i]])
-        y.append(str(result[order[i]]))
-    w = wi.WiSARD(num_bits_addr, False)
+    for i in range(0, int(0.8*len(binary))):
+        X.append(binary[i])
+        y.append(str(result[i]))
+    w = wi.WiSARD(num_bits_addr)
     w.fit(X, y)
 
-    for i in range(int(0.1*len(binary)), len(binary) - 1):
+    for i in range(int(0.8*len(binary)), len(binary) - 1):
         tries = tries + 1
         prediction = w.predict([binary[i]])
         if str(prediction[0]) == str(result[i]):
@@ -65,13 +56,42 @@ def train_and_local_test(num_bits, thorough_test):
 
     if thorough_test:
         print("====================")        
-        print("n_bits: " + str(num_bits_addr) + " | Accuracy: " + str(successes * 100 / tries) + "%")        
+        print("n_bits: " + str(num_bits_addr) + " | Accuracy: " + ("{0:.2f}").format(successes * 100 / float(tries)) + "%")        
     else:
         print("====================")        
         print("Tries: " + str(tries))        
         print("Successes: " + str(successes))        
-        print("Accuracy: " + str(successes * 100 / tries) + "%")        
-        print("====================")        
+        print("Accuracy: " + str(successes * 100 / float(tries)) + "%")        
+        print("====================")
+
+def local_test_cross_validation(num_bits, k_folds, fold_to_test):
+    num_bits_addr = num_bits
+    successes = 0
+    tries = 0
+    X = []
+    y = []
+    binary, result = r.get_binary_passengers('Resources/train.csv')
+
+    fold_size = int(len(binary) / k_folds)
+
+    for i in range(0, fold_size * fold_to_test):
+        X.append(binary[i])
+        y.append(str(result[i]))
+    
+    for i in range((fold_to_test + 1) * fold_size, len(binary)):
+        X.append(binary[i])
+        y.append(str(result[i]))
+
+    w = wi.WiSARD(num_bits_addr)
+    w.fit(X, y)
+
+    for i in range(fold_size * fold_to_test, (fold_to_test + 1) * fold_size):
+        tries = tries + 1
+        prediction = w.predict([binary[i]])
+        if str(prediction[0]) == str(result[i]):
+            successes = successes + 1
+
+    return tries, successes
 
 def print_usage():
     print("Usage: Main.py [T | G]")
@@ -85,16 +105,29 @@ if __name__ == "__main__":
         print_usage()
         sys.exit(0)
     if str(sys.argv[1]) == "T":
-        train_and_local_test(5, False)
+        local_test(20, False)
+    elif str(sys.argv[1]) == "C":
+        k_folds = int(sys.argv[2])
+        total_tries = 0
+        total_successes = 0
+        for i in range(0, k_folds):
+            tries, successes = local_test_cross_validation(8, k_folds, i)
+            total_tries = total_tries + tries
+            total_successes = total_successes + successes
+        print("====================")        
+        print("Tries: " + str(total_tries))        
+        print("Successes: " + str(total_successes))        
+        print("Accuracy: " + str(total_successes * 100 / float(total_tries)) + "%")        
+        print("====================")
     elif str(sys.argv[1]) == "T*":
-        for i in range(1, 40):
-            train_and_local_test(i, True)
+        for i in range(1, 20):
+            local_test(i, True)
     elif str(sys.argv[1]) == "G":
         if len(sys.argv) < 3:
             print_usage()
             sys.exit(0)
         else:
-            train_and_generate_test_prediction(int(sys.argv[2]))
+            generate_test_prediction(int(sys.argv[2]))
     else:
         print_usage()
         sys.exit(0)
