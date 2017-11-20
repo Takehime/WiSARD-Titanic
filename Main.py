e0c5ffd8286dc76a1691fb780810fdf4e6bb2cf0
Vinícius Garcia <vingarciasc@gmail.com>
2017-11-20 17:58:33 -0200

    v0.4.2
    fixed cross-validation

--- Main.py
+++ Main.py
@@ -70,27 +70,57 @@ def local_test_cross_validation(num_bits, k_folds, fold_to_test):
     tries = 0
     X = []
     y = []
+    passengers, res = r.get_passengers('Resources/train.csv')
     binary, result = r.get_binary_passengers('Resources/train.csv')
 
     fold_size = int(len(binary) / k_folds)
 
-    for i in range(0, fold_size * fold_to_test):
-        X.append(binary[i])
-        y.append(str(result[i]))
-    
-    for i in range((fold_to_test + 1) * fold_size, len(binary)):
-        X.append(binary[i])
-        y.append(str(result[i]))
+    X_0 = []
+    y_0 = []
+    X_1 = []
+    y_1 = []
+
+    for i in range(0, len(passengers)):
+        if str(result[i]) == "True":
+            X_1.append(binary[i])
+            y_1.append(result[i])
+        else:
+            X_0.append(binary[i])
+            y_0.append(result[i])
+
+    folds_x = []
+    folds_y = []
+
+    size_fold_0 = int(len(X_0) / k_folds)
+    size_fold_1 = int(len(X_1) / k_folds)
+    for i in range(0, k_folds):
+        fold_x = []
+        fold_y = []
+
+        for j in range(i * size_fold_0, (i+1) * size_fold_0):
+            fold_x.append(X_0[j])
+            fold_y.append(y_0[j])
+        for j in range(i * size_fold_1, (i+1) * size_fold_1):
+            fold_x.append(X_1[j])
+            fold_y.append(y_1[j])
+
+        folds_x.append(fold_x)
+        folds_y.append(fold_y)
+
+    for i in range(0, len(folds_x)):
+        if i != fold_to_test:
+            X.extend(folds_x[i])
+            y.extend(folds_y[i])
 
     w = wi.WiSARD(num_bits_addr)
     w.fit(X, y)
 
-    for i in range(fold_size * fold_to_test, (fold_to_test + 1) * fold_size):
+    prediction = w.predict(folds_x[fold_to_test])
+    for j in range(0, len(prediction)):
         tries = tries + 1
-        prediction = w.predict([binary[i]])
-        if str(prediction[0]) == str(result[i]):
+        if str(prediction[j]) == str(folds_y[fold_to_test][j]):
             successes = successes + 1
-
+        
     return tries, successes
 
 def print_usage():
@@ -98,6 +128,21 @@ def print_usage():
     print("     [T]: Train with 80% of training set and test on the other 20% (num_bits_addr: 5)")
     print("     [T*]: Same as T, but tests with every num_bits_address in the range (0, 40)")
     print("     [G -n]: Train with the entirety of the training set and test on the test set, generating a file 'result.csv' in Resources folder. Using 'n' as num_bits_addr.")
+
+def find_percentiles():
+    passengers, res = r.get_passengers('Resources/train.csv')
+    ages = np.array([])
+    for p in passengers:
+        ages = np.append(ages, p.age)
+    ages.sort()
+
+    print str(ages)
+    print "00%: " + str(np.percentile(ages, 0))
+    print "20%: " + str(np.percentile(ages, 20))
+    print "40%: " + str(np.percentile(ages, 40))
+    print "60%: " + str(np.percentile(ages, 60))
+    print "80%: " + str(np.percentile(ages, 80))
+    print "100%: " + str(np.percentile(ages, 100))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
